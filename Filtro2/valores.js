4d257d685c07bc5e74becc2237cee8fe04a1d42c
Henrique1999 <62816841+Henrique1999@users.noreply.github.com>
2023-03-01 20:44:48 -0300

    Add files via upload

---
+++ Filtro2/valores.js
@@ -0,0 +1,16 @@
+const JSONenviado = {
+  "valores" : [
+    "uva",
+    "limão",
+    "kiwi",
+    "ameixa",
+    "limão",
+    "uva",
+    "cacau",
+    "limão",
+    "cacau",
+    "limão",
+    "cacau",
+  ]
+}
+console.log(JSONenviado);
